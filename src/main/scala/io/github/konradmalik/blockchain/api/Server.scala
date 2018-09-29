b0ca7525a9a9e56b451182332e03e46eb48b7aee
Konrad Malik <konrad.malik@gmail.com>
2018-09-29 12:30:11 +0200

    very simple cluster implementation

--- src/main/scala/io/github/konradmalik/blockchain/api/Server.scala
+++ src/main/scala/io/github/konradmalik/blockchain/api/Server.scala
@@ -1,11 +1,11 @@
 package io.github.konradmalik.blockchain.api
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
-import io.github.konradmalik.blockchain.api.actors.Supervisor
+import io.github.konradmalik.blockchain.api.actors.{BlockchainClusterListener, Supervisor}
 import io.github.konradmalik.blockchain.api.routes.BlockchainRoutes
 import io.github.konradmalik.blockchain.util.TypesafeConfig
 
@@ -13,10 +13,12 @@ import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{Await, ExecutionContextExecutor}
 import scala.io.StdIn
 
-object Server extends App with TypesafeConfig
-  with BlockchainRoutes {
+object Server extends App with TypesafeConfig with BlockchainRoutes {
 
-  implicit val system: ActorSystem = ActorSystem("blockchain-http-service")
+  implicit val system: ActorSystem = ActorSystem("blockchain-http-service", config)
+  // Create an actor that handles cluster domain events
+  val blockchainClusterListener = system.actorOf(Props[BlockchainClusterListener], name = "blockchain-cluster-listener")
+
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
@@ -35,9 +37,10 @@ object Server extends App with TypesafeConfig
   // rest api
   val routes: Route = blockchainRoutes
 
-  val bindingFuture = Http().bindAndHandle(routes, config.getString("http.host"), config.getInt("http.port"))
+  val bindingFuture = Http().bindAndHandle(routes, config.getString("akka.http.host"), config.getInt("akka.http.port"))
 
-  println(s"Server online at http://" + config.getString("http.host") + ":" + config.getInt("http.port") + "\nPress RETURN to stop...")
+  println(s"Server online at http://" + config.getString("akka.http.host") + ":" + config.getInt("akka.http.port") +
+    "\nPress RETURN to stop...")
 
   // stop after ENTER
   StdIn.readLine()
--- src/main/scala/io/github/konradmalik/blockchain/util/TypesafeConfig.scala
+++ src/main/scala/io/github/konradmalik/blockchain/util/TypesafeConfig.scala
@@ -8,6 +8,14 @@ trait TypesafeConfig {
   private val defaultConfig: Config = ConfigFactory.parseResources("defaults.conf")
   private val overridesConfig: Config = ConfigFactory.parseResources("overrides.conf")
     .withFallback(defaultConfig)
+  // overwrite cluster hosts and ports to the ones defined in http service
+  // Override the configuration of the port
+  private val imputedConfig = ConfigFactory.parseString(s"""
+        akka.remote.netty.tcp.hostname=${overridesConfig.getString("akka.http.host")}
+        akka.remote.netty.tcp.port=${overridesConfig.getString("akka.http.port")}
+        akka.remote.artery.canonical.port=${overridesConfig.getString("akka.http.host")}
+        akka.remote.artery.canonical.port=${overridesConfig.getString("akka.http.port")}
+        """).withFallback(overridesConfig)
   // now try to load the conf provided by user, if any
-  val config: Config = ConfigFactory.load.withFallback(overridesConfig)
+  val config: Config = ConfigFactory.load.withFallback(imputedConfig)
 }
