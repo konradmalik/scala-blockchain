03830d518b17c434756d23fc2391cefd6602450c
Konrad Malik <konrad.malik@gmail.com>
2018-09-29 12:55:21 +0200

    fix order in config resolution

--- src/main/scala/io/github/konradmalik/blockchain/util/TypesafeConfig.scala
+++ src/main/scala/io/github/konradmalik/blockchain/util/TypesafeConfig.scala
@@ -8,14 +8,14 @@ trait TypesafeConfig {
   private val defaultConfig: Config = ConfigFactory.parseResources("defaults.conf")
   private val overridesConfig: Config = ConfigFactory.parseResources("overrides.conf")
     .withFallback(defaultConfig)
+  // now try to load the conf provided by user, if any
+  private val applicationConfig: Config = ConfigFactory.load.withFallback(overridesConfig)
   // overwrite cluster hosts and ports to the ones defined in http service
   // Override the configuration of the port
-  private val imputedConfig = ConfigFactory.parseString(s"""
-        akka.remote.netty.tcp.hostname=${overridesConfig.getString("akka.http.host")}
-        akka.remote.netty.tcp.port=${overridesConfig.getString("akka.http.port")}
-        akka.remote.artery.canonical.port=${overridesConfig.getString("akka.http.host")}
-        akka.remote.artery.canonical.port=${overridesConfig.getString("akka.http.port")}
-        """).withFallback(overridesConfig)
-  // now try to load the conf provided by user, if any
-  val config: Config = ConfigFactory.load.withFallback(imputedConfig)
+  val config: Config = ConfigFactory.parseString(s"""
+        akka.remote.netty.tcp.hostname=${applicationConfig.getString("akka.http.host")}
+        akka.remote.netty.tcp.port=${applicationConfig.getString("akka.http.port")}
+        akka.remote.artery.canonical.port=${applicationConfig.getString("akka.http.host")}
+        akka.remote.artery.canonical.port=${applicationConfig.getString("akka.http.port")}
+        """).withFallback(applicationConfig)
 }
